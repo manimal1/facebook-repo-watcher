071844b6e7c671a92cc8a7fb7ba2ad6c6adf4283
McCurdy, Jeremiah <jeremiah.mccurdy@gmail.com>
2018-08-08 08:46:02 +0200

    Add repos pagination

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,1 @@
-export { default as fetchRepos } from './repoActions';
+export { default as repoActions } from './repoActions';
--- src/actions/repoActions.js
+++ src/actions/repoActions.js
@@ -2,9 +2,27 @@ import { repoService } from '../gh-services';
 import { reposConstants } from '../constants';
 
 const fetchRepos = () => async (dispatch) => {
-  const repos = await repoService.fetchAppRepos();
+  const reposInfo = await repoService.fetchAppRepos();
 
-  dispatch({ type: reposConstants.FETCH_REPOS, payload: repos });
+  dispatch({ type: reposConstants.FETCH_REPOS, payload: reposInfo });
 };
 
-export default fetchRepos;
+const fetchReposByPreviousPagination = url => async (dispatch) => {
+  const reposInfo = await repoService.fetchSortedReposByUrl(url);
+
+  dispatch({ type: reposConstants.FETCH_PREVIOUS_REPOS, payload: reposInfo });
+};
+
+const fetchReposByNextPagination = url => async (dispatch) => {
+  const reposInfo = await repoService.fetchSortedReposByUrl(url);
+
+  dispatch({ type: reposConstants.FETCH_NEXT_REPOS, payload: reposInfo });
+};
+
+const repoActions = {
+  fetchRepos,
+  fetchReposByPreviousPagination,
+  fetchReposByNextPagination,
+};
+
+export default repoActions;
